34ac9086766c925da382dda4849ef1b60520d108
kanayan1235 <kininaru5555@gmail.com>
2025-07-06 17:40:43 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+
 document.getElementById('upload').addEventListener('change', function(e) {
   const file = e.target.files[0];
   if (file) {
     const img = document.createElement('img');
     img.src = URL.createObjectURL(file);
-    img.style.maxWidth = '300px';
+    img.onload = () => URL.revokeObjectURL(img.src); // メモリ解放
     document.getElementById('output').innerHTML = '';
     document.getElementById('output').appendChild(img);
   }
