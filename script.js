f82f38823d3a6e4353fe973f1b6d64113d31e4aa
kanayan1235 <kininaru5555@gmail.com>
2025-07-06 17:35:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('さよなら！Myダーリン v5 起動');
+document.getElementById('upload').addEventListener('change', function(e) {
+  const file = e.target.files[0];
+  if (file) {
+    const img = document.createElement('img');
+    img.src = URL.createObjectURL(file);
+    img.style.maxWidth = '300px';
+    document.getElementById('output').innerHTML = '';
+    document.getElementById('output').appendChild(img);
+  }
+});
